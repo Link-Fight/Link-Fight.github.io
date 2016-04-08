79c65c92ccb66c42062380b6527318841dc2ff8a
kelin <linkfight@gmail.com>
2016-04-08 15:51:14 +0800

    task26

---
+++ task/task26/main.js
@@ -0,0 +1,7 @@
+window.onload=function() {
+    var mControl = new Control({
+            AREA: document.getElementById("spaceShipArea"),
+            BOARD: document.getElementById("controlBoard"),
+            ADD: document.getElementById("addSpaceShip"),
+    });
+}
