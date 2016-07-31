9a0fd6134d7ddb7deeb755bd6e9b80ad65ced3dc
kelin <linkfight@gmail.com>
2016-07-31 16:22:23 +0800

    new data

--- Xair/agri3.0Pm/views/demo/chart/chart.js
+++ Xair/agri3.0Pm/views/demo/chart/chart.js
@@ -234,6 +234,113 @@
 	                            }
 	                        ]
 	                    },
+	                    option = {
+	                        title: {
+	                            text: '未来一周气温变化',
+	                            subtext: '纯属虚构',
+	                            x: "center",
+	                        },
+	                        tooltip: {
+	                            trigger: 'axis',
+	                            triggerOn: 'mousemove',
+	                            alwaysShowContentL: true,
+	                            position: function (point, params, dom) {
+	                                // 固定在顶部
+	                                return [point[0], '10%'];
+	                            }
+	                        },
+	                        legend: {
+	                            data: ['最高气温', '最低气温'],
+	                            x: "left",
+	                            padding: 10,
+	                        },
+	                        toolbox: {
+	                            show: true,
+	                            itemSize: 30,
+	                            orient: "vertical",
+	                            feature: {
+	                                dataZoom: {
+	                                    yAxisIndex: 'none'
+	                                },
+	                                dataView: { readOnly: false },
+	                                magicType: { type: ['line', 'bar'] },
+	                                restore: {},
+	                                saveAsImage: {}
+	                            }
+	                        },
+	                        xAxis: {
+	                            type: 'category',
+	                            boundaryGap: true,
+	                            data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+	                        },
+	                        yAxis: {
+	                            type: 'value',
+	                            axisLabel: {
+	                                formatter: '{value} °C'
+	                            }
+	                        },
+	                        dataZoom: [
+	                            {   // 这个dataZoom组件，默认控制x轴。
+	                                type: 'slider', // 这个 dataZoom 组件是 slider 型 dataZoom 组件
+	                                start: 0,      // 左边在 10% 的位置。
+	                                end: 50         // 右边在 60% 的位置。
+	                            },
+	                            {   // 这个dataZoom组件，也控制x轴。
+	                                type: 'inside', // 这个 dataZoom 组件是 inside 型 dataZoom 组件
+	                                start: 10,      // 左边在 10% 的位置。
+	                                end: 60         // 右边在 60% 的位置。
+	                            }
+	                        ],
+	                        series: [
+	                            {
+	                                name: '最高气温',
+	                                type: 'line',
+	                                data: [11, 11, 15, 13, 12, 13, 10],
+	                                markPoint: {
+	                                    data: [
+	                                        { type: 'max', name: '最大值' },
+	                                        { type: 'min', name: '最小值' }
+	                                    ]
+	                                },
+	                                markLine: {
+	                                    data: [
+	                                        { type: 'average', name: '平均值' }
+	                                    ]
+	                                }
+	                            },
+	                            {
+	                                name: '最低气温',
+	                                type: 'line',
+	                                data: [1, -2, 2, 5, 3, 2, 0],
+	                                markPoint: {
+	                                    data: [
+	                                        { name: '周最低', value: -2, xAxis: 1, yAxis: -1.5 }
+	                                    ]
+	                                },
+	                                markLine: {
+	                                    data: [
+	                                        { type: 'average', name: '平均值' },
+	                                        [{
+	                                            symbol: 'none',
+	                                            x: '90%',
+	                                            yAxis: 'max'
+	                                        }, {
+	                                                symbol: 'circle',
+	                                                label: {
+	                                                    normal: {
+	                                                        position: 'start',
+	                                                        formatter: '最大值'
+	                                                    }
+	                                                },
+	                                                type: 'max',
+	                                                name: '最高点'
+	                                            }]
+	                                    ]
+	                                }
+	                            }
+	                        ]
+	                    }
+
 	                ]
 	            }
 	        }
